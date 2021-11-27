3fbce5c95f00f0c6a2f46f1c8b2079fbc461e1eb
ConorButler <cwdbutler@gmail.com>
2021-11-27 18:37:58 +0000

    Adding edge cases for withdrawal

--- test/account.test.js
+++ test/account.test.js
@@ -28,4 +28,20 @@ describe('Account', () => {
     expect(statement).toHaveBeenCalledWith([]);
     // testing that it passes the account history to the statement module
   });
+
+  describe('Edge cases', () => {
+    it('prevents withdrawal when the amount exceeds the balance', () => {
+      bankAccount = new Account(100);
+      expect(() => bankAccount.withdraw(150)).toThrow(
+        'That amount exceeds your balance.'
+      );
+    });
+
+    it('prevents withdrawal when balance is negative', () => {
+      let emptyAccount = new Account();
+      expect(() => emptyAccount.withdraw(1)).toThrow(
+        'That amount exceeds your balance.'
+      );
+    });
+  });
 });
